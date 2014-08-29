941e43f479f72af11c669f7d85812deba05a57c9
Ali Reza <alirezatr@hotmail.com>
2014-08-28 23:11:20 -0400

    Initial creation of project

---
+++ UWMobileTests/UWMobileTests.m
@@ -0,0 +1,34 @@
+//
+//  UWMobileTests.m
+//  UWMobileTests
+//
+//  Created by Ali on 2014-08-28.
+//  Copyright (c) 2014 Ali Ravanchi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UWMobileTests : XCTestCase
+
+@end
+
+@implementation UWMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
