81a3a2629e54951b16f7f19f17de10dbf0c8a438
Ali Reza <alirezatr@hotmail.com>
2014-08-28 23:39:19 -0400

    Set up AFNetworking, wrote example GET request for current weather

--- UWMobile/UWViewController.m
+++ UWMobile/UWViewController.m
@@ -1,4 +1,8 @@
 #import "UWViewController.h"
+#import <AFHTTPRequestOperationManager.h>
+
+
+const NSString *apiKey = @"624b3a3eab10f59832f55b39f6900947";
 
 @interface UWViewController ()
 
@@ -9,13 +13,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self fetchCurrentWeather];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)fetchCurrentWeather {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    [manager GET:@"https://api.uwaterloo.ca/v2/weather/current.json"
+      parameters:nil
+         success:^(AFHTTPRequestOperation *operation, id responseObject) {
+             NSLog(@"JSON: %@", responseObject);
+         }
+         failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+             NSLog(@"Error: %@", error);
+         }];
 }
 
 @end
